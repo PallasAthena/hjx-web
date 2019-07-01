48a8db2100983161aac32b44f727110059447a3c
PallasAthena <okbiqiao@gmail.com>
2019-07-01 11:25:37 +0800

    Updates

---
+++ precache-manifest.fdedc65ad1e440633374573e7017d023.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32b15a76dbd752fd39e52349eddffb11",
+    "url": "/hjx-web/index.html"
+  },
+  {
+    "revision": "2d5351cd194e9d804233",
+    "url": "/hjx-web/static/css/main.d2bff28a.chunk.css"
+  },
+  {
+    "revision": "2e5411b7ef338c769d1a",
+    "url": "/hjx-web/static/js/2.fa79baec.chunk.js"
+  },
+  {
+    "revision": "2d5351cd194e9d804233",
+    "url": "/hjx-web/static/js/main.672b7e7a.chunk.js"
+  },
+  {
+    "revision": "bd63732492de43c09246",
+    "url": "/hjx-web/static/js/runtime~main.69574a4b.js"
+  },
+  {
+    "revision": "8ad9e5de33366c02db8910b59dd68a15",
+    "url": "/hjx-web/static/media/1561713950840.8ad9e5de.jpg"
+  },
+  {
+    "revision": "b8e89c3f6cbe95bfcb2d922ed8c096ed",
+    "url": "/hjx-web/static/media/WX20190628-164744.b8e89c3f.png"
+  },
+  {
+    "revision": "277fe2fe0f7c2ee7245a32db9bbc6689",
+    "url": "/hjx-web/static/media/WX20190628-164803.277fe2fe.png"
+  },
+  {
+    "revision": "3ef620f3ad914f5bbc12fc83f1888447",
+    "url": "/hjx-web/static/media/carousel_1.3ef620f3.png"
+  },
+  {
+    "revision": "15121467befc47a6061c383a4aa2c2a5",
+    "url": "/hjx-web/static/media/carousel_2.15121467.png"
+  },
+  {
+    "revision": "774601fecb7e99565c8d6ef9359e1fbe",
+    "url": "/hjx-web/static/media/carousel_4.774601fe.png"
+  },
+  {
+    "revision": "833bc8c746309280344f7f1908c669d4",
+    "url": "/hjx-web/static/media/carousel_5.833bc8c7.png"
+  }
+]);
